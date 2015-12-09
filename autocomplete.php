af438e57f60b5383aa735b962608489cb17c3a02
reuben <mesolithic@gmail.com>
2015-12-09 17:37:58 +0900

    clean up more code

--- autocomplete.php
+++ autocomplete.php
@@ -1,33 +1,25 @@
 <?php
- 
 if (isset($_GET['term'])){
-		$return_arr = array();
-		 
-try {
-	$config = parse_ini_file('../../sql.ini');
-	$con=mysqli_connect($config['host'],$config['username'],$config['password'],$config['dbname']);
-	if (mysqli_connect_errno()) {
-		          echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-$key=mysqli_real_escape_string($con,$_GET['term']);
-mysqli_set_charset($con,"utf8");
-$query="SELECT jpn,eng,match(jpn,eng,jpn_s,yomi,romaji) against ('".$key."') from allonly7 where match(jpn,eng,jpn_s,yomi,romaji) against ('".$key."') LIMIT 10";
-//echo $query;
-$result = mysqli_query($con,$query);
+    $return_arr = array();
+    try {
+        $config = parse_ini_file('../../sql.ini');
+        $con=mysqli_connect($config['host'],$config['username'],$config['password'],$config['dbname']);
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+        $key=mysqli_real_escape_string($con,$_GET['term']);
+        mysqli_set_charset($con,"utf8");
+        $query="SELECT jpn,eng,match(jpn,eng,jpn_s,yomi,romaji) against ('".$key.
+            "') from allonly7 where match(jpn,eng,jpn_s,yomi,romaji) against ('".$key."') LIMIT 10";
+        $result = mysqli_query($con,$query);
 
-
-
-//	while($row = $stmt->fetch()) {
-while($row = mysqli_fetch_array($result)) {
-
-$return_arr[] =  $row['eng'];
-					    }
-			    	} catch(PDOException $e) {
-						    echo 'ERROR: ' . $e->getMessage();
-										    	}
-		 
-		 
-		    /* Toss back results as json encoded array. */
-		    echo json_encode($return_arr);
+        while($row = mysqli_fetch_array($result)) {
+            $return_arr[] =  $row['eng'];
+        }
+    } catch(PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }
+    /* Toss back results as json encoded array. */
+    echo json_encode($return_arr);
 }
 ?>
--- romajizer.pl
+++ romajizer.pl
@@ -5,7 +5,7 @@ $!="no file";
 open FILE, "kana.txt" or die $!;
 
 while ($line =<FILE>){
-	$romaji = kana2romaji(decode_utf8 $line);
-	print $romaji;
+    $romaji = kana2romaji(decode_utf8 $line);
+    print $romaji;
 }
 
